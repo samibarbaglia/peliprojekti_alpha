089990ffa5731fe1a9d422ac2e1792b845e6bff6
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-12-14 11:20:14 +0200

    username

--- peliprojekti_front/js/main.js
+++ peliprojekti_front/js/main.js
@@ -9,13 +9,3 @@ map.setView([60.1902, 24.5748], 5);
 
 let marker = L.marker([60.1902, 24.5748]).addTo(map);
 
-/* TOINEN KARTTA */
-/* let map = L.map('map').setView([60.1902, 24.5748], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 4,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-let marker = L.marker([60.1902, 24.5748]).addTo(map);
- */
