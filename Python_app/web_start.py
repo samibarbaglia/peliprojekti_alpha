85679a70f1a7a61a48a8a993be783255994fbc53
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-12-14 18:51:45 +0200

    planetype sivu layout

--- Python_app/web_start.py
+++ Python_app/web_start.py
@@ -10,7 +10,7 @@ from collections import deque
 link = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
-    database='flight_game',
+    database='lentopeliprojekti',
     user='root',
     password='p4r!i3',
     autocommit=True
@@ -66,17 +66,38 @@ def plane():
     return render_template_string('''
     {% extends "base.html" %}
             {% block content %}
+            
+            <h1>PLANE TYPES </h1>
             <table>
-            <tr>
-            <h4>Large passenger airplane</h4><p>Kulutus: 62,000 co2-kg/km<br>Max lentomatka: 5556 km</p>
-        <form action="/bg/plane/large" method="post"><button type="submit" value="Choose Large">Choose Large</button></form>
-        </tr>
-        <tr>
-        <h4>Small airplane</h4><p>Kulutus: 0,583 co2-kg/km<br>Max lentomatka: 2778 km</p>
-        <form action="/bg/plane/small" method="post"><button type="submit" value="Choose Small">Choose Small</button>
-        </form>
-        </tr>
-        </table>
+                <tr>
+                    <th scope="col">Large</th>
+                </tr>
+                <tr>
+                    <td>Kulutus: 62,000 co2-kg/km</td>
+                </tr>
+                <tr>
+                    <td>Max. lentomatka: 5556 km</td>
+                </tr>
+                <tr>
+                    <td style=border:none><form action="/bg/plane/large" method="post"><button type="submit" value="Choose Large">CHOOSE LARGE</button></form></td>
+                    </td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <th scope="col">Small</th>
+                </tr>
+                <tr>
+                    <td>Kulutus: 0,583 co2-kg/km</td>
+                </tr>
+                <tr>
+                    <td>Max. lentomatka: 2778 km</td>
+                </tr>
+                <tr>
+                    <td style=border:none><form action="/bg/plane/small" method="post"><button type="submit" value="Choose Small">CHOOSE SMALL</button>
+                    </form></td>
+                </tr>
+            </table>
         
     {% endblock %}''')
 
