7166522e51716111f8bf40ab7b1650b681b42167
reimakoo <reimaku@metropolia.fi>
2022-12-14 14:56:01 +0200

    vittu jee

--- peliprojekti_front/js/main.js
+++ peliprojekti_front/js/main.js
@@ -1,5 +1,5 @@
 'use strict'
-const map = L.map('map', {tap: false });
+const map = L.map('map', {tap: false});
 
 L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
   maxZoom: 20,
@@ -7,12 +7,33 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
 }).addTo(map);
 map.setView([60.1902, 24.5748], 5);
 
-let marker = L.marker([60.1902, 24.5748]).addTo(map);
+const marker = L.marker([60.1902, 24.5748]).addTo(map);
+
 
 const username = localStorage.getItem('username');
 if (username) {
   const li = document.querySelector('div.stats li:first-child');
   li.innerHTML = username;
-
 }
 
+
+
+async function gameSetup() {
+  try {
+    const gameData = await getData('testdata/newgame.json');
+    console.log(gameData)
+
+    for(let airport of gameData.location) {
+      const marker = L.marker([airport.latitude, airport.longitude]).addTo(map);
+      if(airport.active === true) {
+        marker.bindPopup(`Current location: <b>${airport.name}</b>`);
+        marker.openPopup();
+      }
+    }
+
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+gameSetup();
