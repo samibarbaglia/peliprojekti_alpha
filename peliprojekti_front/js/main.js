d09b2129bf8038433acf162213ca83b216d31b3e
reimakoo <reimaku@metropolia.fi>
2022-12-14 13:44:43 +0200

    vittu jee

--- peliprojekti_front/js/main.js
+++ peliprojekti_front/js/main.js
@@ -9,3 +9,9 @@ map.setView([60.1902, 24.5748], 5);
 
 let marker = L.marker([60.1902, 24.5748]).addTo(map);
 
+const username = localStorage.getItem('username');
+if (username) {
+  const li = document.querySelector('div.stats li:first-child');
+  li.innerHTML = username;
+}
+
